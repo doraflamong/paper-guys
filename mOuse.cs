2c35aed1ed5643b9041376c4e5debe030f6695e7
doraflamong <69072876+doraflamong@users.noreply.github.com>
2021-12-31 11:10:53 +0800

    ew
    ew

---
+++ mOuse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+
+    public float MouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = Cur
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * TIme.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f , 90f);
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(vector3.up * mouseX);
+    }
+}
